d721543d14a8a079a1c0ab1daec2b07aee37b507
Joshua Barrass <josh.barrass.work@gmail.com>
2025-04-23 17:43:48 +0100

    Change error message

--- module_loader.c
+++ module_loader.c
@@ -2,9 +2,8 @@
 #error "You are not using a cross-compiler, you will most certainly run into trouble"
 #endif
 
-/* This tutorial will only work for the 32-bit ix86 targets. */
 #if !defined(__i386__)
-#error "This tutorial needs to be compiled with a ix86-elf compiler"
+#error "The module loader needs to be compiled with a ix86-elf compiler"
 #endif
 
 #include "multiboot.h"
