620a25237742ed1ad646d0dec472b8882b02be9b
Joshua Barrass <josh.barrass.work@gmail.com>
2025-04-23 22:10:30 +0100

    Declare at boot whether the OS is 32-bit or 64-bit

--- os.c
+++ os.c
@@ -8,7 +8,12 @@
 
 #include "tty.h"
 
-static const char *welcomeMessage = "Welcome to jOSh!";
+static const char *welcomeMessage = "Welcome to jOSh! "
+#if defined(__x86_64__) || defined(_M_X64)
+  "(64-bit)";
+#elif defined(i386) || defined(__i386__) || defined(__i386) || defined(_M_IX86)
+  "(32-bit)";
+#endif
 
 void kernel_main() {
   clear_screen();
