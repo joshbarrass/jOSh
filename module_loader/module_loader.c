e30eed1e4d6c57b29cf1b9295f70773f14fc7195
Joshua Barrass <josh.barrass.work@gmail.com>
2025-04-24 14:05:14 +0100

    Simplify code by only testing the first module
    This requires that the kernel be passed in as the first module by the
    multiboot loader. We can potentially also use this to relay any
    additional modules to the kernel after loading by just dropping it
    from the array.

--- module_loader/module_loader.c
+++ module_loader/module_loader.c
@@ -28,7 +28,7 @@ void module_loader_main() {
   ++yline;
 
   if (mis->FLAGS & (1 << 3) && mis->mods_count > 0) {
-    print_string("[+] Modules are available", 0, yline);
+    print_string("[+] Modules are available. Testing module 0...", 0, yline);
     ++yline;
   } else {
     terminal_color.fg = 4;
@@ -36,32 +36,32 @@ void module_loader_main() {
     return;
   }
 
-  for (size_t i = 0; i < mis->mods_count; ++i) {
-    const char *mod = (char*)mis->mods[i].mod_start;
-    print_string("    * ", 0, yline);
-    print_string(mis->mods[i].string, 6, yline);
-    ++yline;
-    if (!is_ELF(mod)) {
-      print_string("      Unknown format", 0, yline);
-      ++yline;
-      continue;
-    }
-    print_string("      ELF ??-bit ??", 0, yline);
-    if (get_ELF_class(mod) == EI_CLASS_32BIT) {
-      print_string("32", 10, yline);
-    } else if (get_ELF_class(mod) == EI_CLASS_64BIT) {
-      print_string("64", 10, yline);
-    }
-    if (get_ELF_endianness(mod) == EI_ENDIANNESS_LITTLE) {
-      print_string("LE", 17, yline);
-    } else if (get_ELF_endianness(mod) == EI_ENDIANNESS_BIG) {
-      print_string("BE", 17, yline);
-    }
-    ++yline;
-  }
-
-  // load the first module
+  // verify that the first module in the list is an ELF file
   const char *mod = (char*)mis->mods[0].mod_start;
+  print_string("    * ", 0, yline);
+  print_string(mis->mods[0].string, 6, yline);
+  ++yline;
+  if (!is_ELF(mod)) {
+    terminal_color.fg = 4;
+    print_string("      Unknown format", 0, yline);
+    ++yline;
+    return;
+  }
+  // print the ELF info
+  print_string("      ELF ??-bit ??", 0, yline);
+  if (get_ELF_class(mod) == EI_CLASS_32BIT) {
+    print_string("32", 10, yline);
+  } else if (get_ELF_class(mod) == EI_CLASS_64BIT) {
+    print_string("64", 10, yline);
+  }
+  if (get_ELF_endianness(mod) == EI_ENDIANNESS_LITTLE) {
+    print_string("LE", 17, yline);
+  } else if (get_ELF_endianness(mod) == EI_ENDIANNESS_BIG) {
+    print_string("BE", 17, yline);
+  }
+  ++yline;
+
+  // load the module
   if (get_ELF_class(mod) == EI_CLASS_32BIT) {
     elf32_build_program_image(mod);
     print_string("[+] ELF Loaded!", 0, yline);
