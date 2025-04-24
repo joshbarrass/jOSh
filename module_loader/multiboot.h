e8b11b8c9195bb3de4fa6800eb0478041e51498d
Joshua Barrass <josh.barrass.work@gmail.com>
2025-04-24 18:17:51 +0100

    Helper functions to ensure MIS works in 32-bit and 64-bit
    The MIS containers pointers, which will change size when we switch
    from 32-bit to 64-bit. This would make the MIS unreadable in 64-bit
    mode. To work around this, we test for the architecture and either
    define the struct with real pointers, or define it with fixed-size
    integers. We then provide helper functions that will always return
    valid pointers, both in 32-bit and 64-bit modes.

--- module_loader/multiboot.h
+++ module_loader/multiboot.h
@@ -5,14 +5,35 @@
 #include <stddef.h>
 #include <stdbool.h>
 
-typedef struct Mod {
+#if defined(__x86_64__) || defined(_M_X64)
+#define ARCH_64 1
+#elif defined(i386) || defined(__i386__) || defined(__i386) || defined(_M_IX86)
+#define ARCH_32 1
+#endif
+
+#ifdef ARCH_32
+typedef struct __attribute__((packed)) {
   void *mod_start;
   void *mod_end;
   char *string;
   uint32_t _reserved;
 } Mod;
+inline void *get_mod_start(Mod *m) { return m->mod_start; }
+inline void *get_mod_end(Mod *m) { return m->mod_end; }
+inline char *get_mod_string(Mod *m) { return m->string; }
+#else
+typedef struct __attribute__((packed)) {
+  uint32_t mod_start;
+  uint32_t mod_end;
+  uint32_t string;
+  uint32_t _reserved;
+} Mod;
+inline void *get_mod_start(Mod *m) { return (void*)(m->mod_start); }
+inline void *get_mod_end(Mod *m) { return (void*)(m->mod_end); }
+inline char *get_mod_string(Mod *m) { return (char*)(m->string); }
+#endif
 
-typedef struct MIS {
+typedef struct __attribute__((packed)) {
   uint32_t FLAGS;
 
   uint32_t mem_lower;
@@ -20,12 +41,27 @@ typedef struct MIS {
 
   uint32_t boot_device;
 
+  #ifdef ARCH_32
   char *cmdline;
+  #else
+  uint32_t cmdline;
+  #endif
 
   uint32_t mods_count;
+  #ifdef ARCH_32
   Mod *mods;
+  #else
+  uint32_t mods;
+  #endif
   
   // TODO: rest of the header
 } MIS;
+#ifdef ARCH_32
+inline char *get_cmdline(MIS *m) { return m->cmdline; }
+inline Mod *get_mods(MIS *m) { return m->mods; }
+#else
+inline char *get_cmdline(MIS *m) { return (char*)(m->cmdline); }
+inline Mod *get_mods(MIS *m) { return (Mod*)(m->mods); }
+#endif
 
 #endif
