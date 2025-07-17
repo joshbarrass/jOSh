df5e5df6c8d6fcea340393e3f165b9c336672ef0
Joshua Barrass <josh.barrass.work@gmail.com>
2025-07-17 02:01:02 +0100

    Basic working strtoumax and strtoimax (base 10 only)
    Goes via a common strtoany function that should be easy to extend to
    the other cases. The trick here is specifying whether or not the max
    value is that of a signed or unsigned type; this allows the correct
    extreme value to be returned if an overflow should occur by exploiting
    two's complement, i.e. INTMAX_MAX = abs(INTMAX_MIN)-1
    
    Created some simple tests around the extrema, and the implementation
    appears to behave correctly.

---
+++ libc/stdlib/strto/strtoumax.c
@@ -0,0 +1,77 @@
+#include <stdbool.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <errno.h>
+
+uintmax_t strtoany(const char *restrict s, char **restrict endptr, int base,
+                   uintmax_t max_val, bool is_signed) {
+  *endptr = s;
+  // skip leading whitespace
+  while (isspace(*s)) {
+    ++s;
+  }
+
+  // parse the sign (if present)
+  bool negative = false;
+  if (s[0] == '-') {
+    negative = true;
+    ++s;
+  } else if (s[0] == '+') {
+    ++s;
+  }
+
+  // short circuit now if the string contains no numerical chars
+  if (*s < '0' || *s > '9') {
+    return 0;
+  }
+
+  uintmax_t n = 0;
+  uintmax_t n_prev = 0;
+  bool overflowed = false;
+  while (*s >= '0' && *s <= '9') {
+    // C99 standard says sign is applied to the parsed value at the
+    // end, rather than applying it now, so the issue around
+    // INT_MAX/INT_MIN should be an expected edge case as part of the
+    // standard
+    n = 10*n + (*s - '0');
+
+    // detect overflow
+    if (n > max_val || n < n_prev) {
+      overflowed = true;
+    }
+    ++s;
+  }
+
+  *endptr = s;
+  if (overflowed) {
+    errno = ERANGE;
+    if (!negative && is_signed) {
+      // if the type is signed and a positive value overflowed, return
+      // max_val-1, which is equal to the max signed value for the
+      // type in two's complement
+      return max_val-1;
+    } else {
+      // otherwise either:
+      //   1) the type is signed and the value is negative, in which
+      //   case we return max_val = TYPE_MIN
+      // OR
+      //   2) the type is not signed, in which case we should return
+      //   max_val = TYPE_MAX
+      return max_val;
+    }
+  }
+  // we didn't overflow, so follow the C standard regarding applying
+  // the sign
+  if (negative) {
+    return -(uintmax_t)n;
+  }
+  return n;
+}
+
+uintmax_t strtoumax(const char *restrict s, char **restrict endptr, int base) {
+  return strtoany(s, endptr, base, UINTMAX_MAX, false);
+}
+
+intmax_t strtoimax(const char *restrict s, char **restrict endptr, int base) {
+  return strtoany(s, endptr, base, INTMAX_MIN, true);
+}
