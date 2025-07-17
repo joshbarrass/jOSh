9a3694f06fff7a351dadbbfe3e4f4989e59dbdde
Joshua Barrass <josh.barrass.work@gmail.com>
2025-07-17 02:01:02 +0100

    Basic working strtoumax and strtoimax (base 10 only)
    Goes via a common strtoany function that should be easy to extend to
    the other cases. The trick here is specifying whether or not the max
    value is that of a signed or unsigned type; this allows the correct
    extreme value to be returned if an overflow should occur by exploiting
    two's complement, i.e. INTMAX_MAX = abs(INTMAX_MIN)-1
    
    Created some simple tests around the extrema, and the implementation
    appears to behave correctly.

--- libc/include/stdlib.h
+++ libc/include/stdlib.h
@@ -1,8 +1,13 @@
 #ifndef _STDLIB_H
 #define _STDLIB_H
 
+#include <stdint.h>
+
 int atoi(const char *s);
 long int atol(const char *s);
 long long int atoll(const char *s);
+
+uintmax_t strtoumax(const char * restrict s, char ** restrict endptr, int base);
+intmax_t strtoimax(const char * restrict s, char ** restrict endptr, int base);
 
 #endif
