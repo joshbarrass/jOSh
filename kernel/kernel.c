efc5b875299d413aa35079b60320188015b025ef
Joshua Barrass <josh.barrass.work@gmail.com>
2025-07-22 17:22:56 +0100

    Print memory map info
    Adapted from old memmap branch

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -19,6 +19,7 @@
 #include <multiboot.h>
 #include <kernel/panic.h>
 #include <archdef.h>
+#include <kernel/mmap.h>
 
 const MIS *mis = NULL;
 
@@ -34,12 +35,61 @@ static void print_welcome_message() {
          );
 }
 
+inline static void term_info_color() {
+  term_set_fg(VGA_COLOR_LIGHT_GREY);
+}
+
+inline static void term_good_color() {
+  term_set_fg(VGA_COLOR_LIGHT_GREEN);
+}
+
+inline static void term_error_color() {
+  term_set_fg(VGA_COLOR_RED);
+}
+
 void kernel_main() {
   VGA_set_blink(false);
+  term_info_color();
   term_clear_screen();
   print_welcome_message();
-  if (mis != NULL) {
-    printf("%s\n\n", get_mod_string(&get_mods(mis)[0]));
+  if (mis == NULL) {
+    term_error_color();
+    printf("[!] Missing multiboot information struct!\n");
+    return;
   }
+
+  printf("[*] Checking required multiboot flags...\n");
+  if (mis->FLAGS & MULTIBOOT_FLAG_FULL_MMAP) {
+    term_good_color();
+    printf("[+] Memory map is available!\n");
+    term_info_color();
+  } else {
+    term_error_color();
+    printf("[!] Memory map unavailable!\n");
+    return;
+  }
+
+  // ensure the memory map has not been corrupted
+  // if below 2MB, none of our loaded data could have collided with it
+  if (mis->mmap >= 2 * 1024 * 1024 || mis->mmap + mis->mmap_length >= 2 * 1024 * 1024) { 
+    term_error_color();
+    printf("[!] Memory map corrupted!\n");
+    return;
+  } else {
+    term_good_color();
+    printf("[+] Intact mmap found at %#018zx\n", mis->mmap);
+    term_info_color();
+  }
+
+  // print the memory map
+  printf("    |       start       -         end       |type| raw|\n");
+  size_t adv = 0;
+  mmap *map = get_mmap(mis);
+  for (size_t i = 0; i < mis->mmap_length; i += adv) {
+    mmap *entry = (mmap*)(&((char*)map)[i]);
+    adv = entry->size+sizeof(entry->size);
+    printf("    |0x%016zX - 0x%016zX|%s|0x%02x|\n", entry->base_addr, entry->base_addr+entry->length-1, get_mmap_type_string(entry->type), entry->type);
+  }
+
   return;
 }
