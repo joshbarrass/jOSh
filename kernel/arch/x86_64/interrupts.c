14c9adc6a99069addf6fbd360c4fcdbe61d7db83
Joshua Barrass <josh.barrass.work@gmail.com>
2025-08-26 23:56:44 +0100

    Parse the interrupt stack frame to print debug info

--- kernel/arch/x86_64/interrupts.c
+++ kernel/arch/x86_64/interrupts.c
@@ -9,9 +9,37 @@ static struct {
   uintptr_t offset;
 } __attribute__((packed)) IDTD;
 
-static void build_gate_descriptor_64(GateDescriptor64 *gd, void *interrupt,
-                                     uint16_t segment, char ist, char gate_type,
-                                     char dpl, bool present) {
+typedef struct __attribute__((packed)) {
+  uint64_t r15;
+  uint64_t r14;
+  uint64_t r13;
+  uint64_t r12;
+  uint64_t r11;
+  uint64_t r10;
+  uint64_t r9;
+  uint64_t r8;
+  uint64_t rbp;
+  uint64_t rsp;
+  uint64_t rdi;
+  uint64_t rsi;
+  uint64_t rdx;
+  uint64_t rcx;
+  uint64_t rbx;
+  uint64_t rax;
+
+  uint64_t err_code;
+  // based on https://wiki.osdev.org/Interrupt_Service_Routines#Clang
+  uint64_t ret_ip;
+  uint64_t ret_cs;
+  uint64_t ret_flags;
+  uint64_t ret_sp;
+  uint64_t ret_ss;
+} InterruptStackFrame;
+
+static void
+build_gate_descriptor_64(GateDescriptor64 *gd, void *interrupt,
+                         uint16_t segment, char ist, char gate_type, char dpl,
+                         bool present) {
   uint64_t offset = (uint64_t)(uintptr_t)interrupt;
   gd->offset_0 = offset & 0xffffffff;
   offset >>= 16;
@@ -31,8 +59,29 @@ static void build_gate_descriptor_64(GateDescriptor64 *gd, void *interrupt,
 }
 
 __attribute__ ((sysv_abi))
-void do_df() {
-  kpanic("Double fault!");
+void do_df(InterruptStackFrame *fr) {
+  kpanic("Double fault!\n"
+         "Error code: %llx\n"
+         "Fault occured executing: %#018llx\n\n"
+         "Registers:\n"
+         "rax: %#018llx  rbx: %#018llx\n"
+         "rcx: %#018llx  rdx: %#018llx\n"
+         "rsi: %#018llx  rdi: %#018llx\n"
+         "rsp: %#018llx  rbp: %#018llx\n"
+         " r8: %#018llx   r9: %#018llx\n"
+         "r10: %#018llx  r11: %#018llx\n"
+         "r12: %#018llx  r13: %#018llx\n"
+         "r14: %#018llx  r15: %#018llx\n"
+         " cs: %#018llx   ss: %#018llx\n\n"
+         "CPU Flags: %#018llx\n",
+         fr->err_code,
+         fr->ret_ip, fr->rax, fr->rbx,
+         fr->rcx, fr->rdx, fr->rsi,
+         fr->rdi, fr->ret_sp, fr->rbp,
+         fr->r8, fr->r9, fr->r10,
+         fr->r11, fr->r12, fr->r13,
+         fr->r14, fr->r15, fr->ret_cs,
+         fr->ret_ss, fr->ret_flags);
   return;
 }
 
