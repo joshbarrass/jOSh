2ca84e5756f7fb32f9fe160bcab1442613c4ee87
Joshua Barrass <josh.barrass.work@gmail.com>
2025-11-11 00:52:10 +0000

    Add functions for reading CR0, CR2 and CR4

--- kernel/arch/x86_64/control_registers.c
+++ kernel/arch/x86_64/control_registers.c
@@ -1,5 +1,23 @@
 #include <kernel/x86_64/control_registers.h>
 
+uint64_t read_CR0() {
+  uint64_t cr0;
+  asm volatile (
+                "mov %%cr0, %0\r\n"
+                : "=r" (cr0)
+                );
+  return cr0;
+}
+
+uint64_t read_CR2() {
+  uint64_t cr2;
+  asm volatile (
+                "mov %%cr2, %0\r\n"
+                : "=r" (cr2)
+                );
+  return cr2;
+}
+
 uint64_t read_CR3() {
   uint64_t cr3;
   asm volatile (
@@ -7,4 +25,13 @@ uint64_t read_CR3() {
                 : "=r" (cr3)
                 );
   return cr3;
+}
+
+uint64_t read_CR4() {
+  uint64_t cr4;
+  asm volatile (
+                "mov %%cr4, %0\r\n"
+                : "=r" (cr4)
+                );
+  return cr4;
 }
