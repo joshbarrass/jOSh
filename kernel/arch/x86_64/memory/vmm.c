433776fd6ef70a43f0de3d672ba6c741e9b4a295
Joshua Barrass <josh.barrass.work@gmail.com>
2025-11-12 23:21:30 +0000

    Earliest vmm init procedures

--- kernel/arch/x86_64/memory/vmm.c
+++ kernel/arch/x86_64/memory/vmm.c
@@ -1,13 +1,19 @@
 #include <kernel/memory/vmm.h>
 #include <kernel/x86_64/memory/recursive_pt.h>
+#include <kernel/memory/constants.h>
+
+static uintptr_t KSPACE_PDPT_PHYS_ADDR;
 
 void vmm_init() {
   // We must fetch and store the physical address of the kernel-space
-  // page table and recursive entry via the recursive entry. We will
-  // need this to copy it into all new address spaces.
+  // page table via the recursive entry. We will need this to copy it
+  // into all new address spaces.
 
   // Kernel-space in x86_64 lives in a single PDPT (a single 512GB
   // block), so we need to save the address of this single PDPT.
-
-  
+  const PageTableEntry *PML4T = get_PML4T();
+  // convert the kernel start address to an index
+  const ptindex_t kspace_i = (KERNEL_VADDR_START & (511ULL << 39)) >> 39;
+  // bitmask removes the properties above and below the physical address
+  KSPACE_PDPT_PHYS_ADDR = PML4T[kspace_i] & 0xffffffffff000ULL;
 }
