3512bc7f2c25e83e7bd8789b3640e1b8502451da
Joshua Barrass <josh.barrass.work@gmail.com>
2025-11-12 22:34:09 +0000

    Move the recursive entry code to its own file

--- kernel/arch/x86_64/memory/vmm.c
+++ kernel/arch/x86_64/memory/vmm.c
@@ -1,66 +1,13 @@
 #include <kernel/memory/vmm.h>
+#include <kernel/x86_64/memory/paging.h>
 
-enum PageTableLevel {
-  PML4T = 3,
-  PDPT = 2,
-  PD = 1,
-  PT = 0
-};
-typedef enum PageTableLevel PageTableLevel;
+void vmm_init() {
+  // We must fetch and store the physical address of the kernel-space
+  // page table and recursive entry via the recursive entry. We will
+  // need this to copy it into all new address spaces.
 
-typedef uint64_t PageTableEntry;
+  // Kernel-space in x86_64 lives in a single PDPT (a single 512GB
+  // block), so we need to save the address of this single PDPT.
 
-typedef unsigned short ptindex_t;
-
-// x86-64 addresses are 48 bits, but are stored as sign-extended
-// 64-bit values. This function performs the sign extension
-// (canonicalisation) of a pointer.
-inline __attribute__((always_inline)) uintptr_t canonicalise_addr(uintptr_t addr) {
-  return ((int64_t)addr << 16) >> 16;
-}
-
-// returns &CR3[i][j][k][l] if level == PT
-// returns &CR3[i][j][k]    if level == PD
-// returns &CR3[i][j]       if level == PDPT
-// returns &CR3[i]          if level == PML4T
-PageTableEntry *get_page_table_pointer(const PageTableLevel level, const ptindex_t i, const ptindex_t j, const ptindex_t k, const ptindex_t l) {
-  ptindex_t indices[4] = {l, k, j, i};
   
-  uintptr_t base_addr = 0;
-
-  for (int i = 0; i < 4; ++i) {
-    if (i <= level) {
-      base_addr |= 511;
-    } else {
-      base_addr |= (indices[i] & 511);
-    }
-    base_addr <<= 9ULL;
-  }
-  base_addr <<= 3ULL;
-
-  return (PageTableEntry*)canonicalise_addr(base_addr + 8*indices[level]);
-}
-
-// Returns the virtual address corresponding to (*PageTableEntry)CR3
-// This can be treated as a PageTableEntry[512]
-PageTableEntry *get_PML4T() {
-  return get_page_table_pointer(PML4T, 0, 0, 0, 0);
-}
-
-// Returns the virtual address corresponding to (**PageTableEntry)CR3[PML4T_i]
-// This can be treated as a PageTableEntry[512]
-PageTableEntry *get_PDPT(const ptindex_t PML4T_i) {
-  return get_page_table_pointer(PDPT, PML4T_i, 0, 0, 0);
-}
-
-// Returns the virtual address corresponding to (***PageTableEntry)CR3[PML4T_i][PDPT_i]
-// This can be treated as a PageTableEntry[512]
-PageTableEntry *get_PD(const ptindex_t PML4T_i, const ptindex_t PDPT_i) {
-  return get_page_table_pointer(PD, PML4T_i, PDPT_i, 0, 0);
-}
-
-// Returns the virtual address corresponding to (****PageTableEntry)CR3[PML4T_i][PDPT_i][PD_i]
-// This can be treated as a PageTableEntry[512]
-PageTableEntry *get_PT(const ptindex_t PML4T_i, const ptindex_t PDPT_i, const ptindex_t PD_i) {
-  return get_page_table_pointer(PT, PML4T_i, PDPT_i, PD_i, 0);
 }
