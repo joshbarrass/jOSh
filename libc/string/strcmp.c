db323f344a6c8a4c96fb40984a032867d85d701a
Joshua Barrass <josh.barrass.work@gmail.com>
2025-12-01 10:29:40 +0000

    Implement strcmp

---
+++ libc/string/strcmp.c
@@ -0,0 +1,13 @@
+#include <string.h>
+
+int strcmp(const char *s1, const char *s2) {
+  while (*s1 != 0 && *s2 != 0) {
+    if (*s1 > *s2) return 1;
+    if (*s1 < *s2) return -1;
+    ++s1;
+    ++s2;
+  }
+  if (*s1 > *s2) return 1;
+  if (*s1 < *s2) return -1;
+  return 0;
+}
