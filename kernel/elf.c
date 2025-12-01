bf746d0c3f7646ded325b441e06aa5de20094f37
Joshua Barrass <josh.barrass.work@gmail.com>
2025-12-01 10:29:51 +0000

    Implement loading an ELF using vmm_kmap

---
+++ kernel/elf.c
@@ -0,0 +1,111 @@
+#include <elf.h>
+#include <kernel/memory/vmm.h>
+#include <kernel/memory/pmm.h>
+#include <kernel/memory/constants.h>
+#include <string.h>
+
+bool is_ELF(const char * const addr) {
+  return (addr[EI_MAG] == 0x7F && addr[EI_MAG+1] == 'E' && addr[EI_MAG+2] == 'L' && addr[EI_MAG+3] == 'F');
+}
+
+char get_ELF_class(const char * const addr) {
+  return addr[EI_CLASS];
+}
+
+char get_ELF_endianness(const char * const addr) {
+  return addr[EI_ENDIANNESS];
+}
+
+/* void elf32_build_program_image(const char *const elf) { */
+/*   const Elf32_Ehdr * const header = (const Elf32_Ehdr * const)elf; */
+/*   const uint64_t p = (uint64_t)(uintptr_t)elf + (uint64_t)(uintptr_t)header->e_phoff; */
+/*   const Elf32_Phdr * const pheader = (const Elf32_Phdr * const)(uintptr_t)p; */
+
+/*   for (size_t i = 0; i < header->e_phnum; ++i) { */
+/*     const void *const segment_start = (const void *const)(pheader[i].p_offset + elf); */
+/*     void *const virt_addr = (void *const)(uintptr_t)pheader[i].p_vaddr; */
+/*     const size_t fsize = (size_t)pheader[i].p_filesz; */
+/*     const size_t msize = (size_t)pheader[i].p_memsz; */
+
+/*     vmm_kmap(segment_start,  */
+/*     memmove(virt_addr, segment_start, fsize); */
+/*     for (int i = fsize; i < msize; ++i) { */
+/*       ((char*)virt_addr)[i] = 0; */
+/*     } */
+/*   } */
+/* } */
+
+void elf64_build_program_image(const char *const elf) {
+  const Elf64_Ehdr * const header = (const Elf64_Ehdr * const)elf;
+  const uint64_t p = (uint64_t)(uintptr_t)elf + (uint64_t)(uintptr_t)header->e_phoff;
+  const Elf64_Phdr * const pheader = (const Elf64_Phdr * const)(uintptr_t)p;
+
+  for (size_t i = 0; i < header->e_phnum; ++i) {
+    const void *const segment_start = (const void *const)(pheader[i].p_offset + elf);
+    void *const virt_addr = (void *const)(uintptr_t)pheader[i].p_vaddr;
+    const size_t fsize = (size_t)pheader[i].p_filesz;
+    const size_t msize = (size_t)pheader[i].p_memsz;
+
+    // find how much of the segment is not page-aligned
+    const size_t size_to_map = (fsize / PAGE_SIZE) * PAGE_SIZE;
+    const size_t physical_remainder = fsize % PAGE_SIZE;
+    // map the page-aligned chunk directly
+    vmm_kmap((uintptr_t)segment_start, size_to_map, (uintptr_t)virt_addr, 0);
+    if (physical_remainder != 0) {
+      // copy the remainder to a new page and map that contiguously
+      const uintptr_t newpage = (uintptr_t)pmm_alloc_pages(1);
+      vmm_kmap(newpage, PAGE_SIZE, (uintptr_t)virt_addr + size_to_map, 0);
+      memmove((void*)((uintptr_t)virt_addr + size_to_map), segment_start + size_to_map, physical_remainder);
+      for (int i = physical_remainder; i < PAGE_SIZE; ++i) {
+        ((char*)((uintptr_t)virt_addr + size_to_map))[i] = 0;
+      }
+    }
+
+    // generate enough pages for the remaining msize
+    const size_t virtual_excess = msize - fsize;
+    size_t pages_needed = virtual_excess / PAGE_SIZE;
+    const size_t rem = virtual_excess % PAGE_SIZE;
+    if (rem > 0) pages_needed += 1;
+    const uintptr_t newpage = (uintptr_t)pmm_alloc_pages(pages_needed);
+    vmm_kmap(newpage, pages_needed*PAGE_SIZE, (uintptr_t)virt_addr + fsize, 0);
+  }
+}
+
+Elf32_Addr get_elf32_entrypoint(const char *const elf) {
+  const Elf32_Ehdr * const header = (const Elf32_Ehdr * const)elf;
+  return header->e_entry;
+}
+Elf64_Addr get_elf64_entrypoint(const char *const elf) {
+  const Elf64_Ehdr * const header = (const Elf64_Ehdr * const)elf;
+  return header->e_entry;
+}
+
+Elf32_Addr get_elf32_lowest_addr(const char *const elf) {
+  const Elf32_Ehdr * const header = (const Elf32_Ehdr * const)elf;
+  const uint64_t p = (uint64_t)(uintptr_t)elf + (uint64_t)(uintptr_t)header->e_phoff;
+  const Elf32_Phdr * const pheader = (const Elf32_Phdr * const)(uintptr_t)p;
+  Elf32_Addr lowest_addr = 0xffffffff;
+
+  for (size_t i = 0; i < header->e_phnum; ++i) {
+    Elf32_Addr virt_addr = pheader[i].p_vaddr;
+    if (virt_addr < lowest_addr) {
+      lowest_addr = virt_addr;
+    }
+  }
+  return lowest_addr;
+}
+
+Elf64_Addr get_elf64_lowest_addr(const char *const elf) {
+  const Elf64_Ehdr * const header = (const Elf64_Ehdr * const)elf;
+  const uint64_t p = (uint64_t)(uintptr_t)elf + (uint64_t)(uintptr_t)header->e_phoff;
+  const Elf64_Phdr * const pheader = (const Elf64_Phdr * const)(uintptr_t)p;
+  Elf64_Addr lowest_addr = 0xffffffffffffffff;
+
+  for (size_t i = 0; i < header->e_phnum; ++i) {
+    Elf64_Addr virt_addr = pheader[i].p_vaddr;
+    if (virt_addr < lowest_addr) {
+      lowest_addr = virt_addr;
+    }
+  }
+  return lowest_addr;
+}
---
+++ kernel/include/elf.h
@@ -0,0 +1,109 @@
+#ifndef __ELF_H
+#define __ELF_H
+
+/* Source: http://www.skyfree.org/linux/references/ELF_Format.pdf
+
+   Adapted from above based on
+   https://en.wikipedia.org/wiki/Executable_and_Linkable_Format#ELF_header */
+
+#include <stdint.h>
+#include <stdbool.h>
+#include <stddef.h>
+
+typedef uint32_t Elf32_Addr;
+typedef uint16_t Elf32_Half;
+typedef uint32_t Elf32_Off;
+typedef  int32_t Elf32_Sword;
+typedef uint32_t Elf32_Word;
+
+#define EI_NIDENT 16
+#define EI_MAG 0x0
+#define EI_CLASS 0x4
+#define EI_ENDIANNESS 0x5
+#define EI_VERSION 0x6
+#define EI_OSABI 0x7
+#define EI_ABIVERSION 0x8
+#define EI_PAD 0x9
+
+#define EI_CLASS_32BIT 1
+#define EI_CLASS_64BIT 2
+
+#define EI_ENDIANNESS_LITTLE 1
+#define EI_ENDIANNESS_BIG 2
+
+typedef struct {
+unsigned char e_ident[EI_NIDENT];
+Elf32_Half e_type;
+Elf32_Half e_machine;
+Elf32_Word e_version;
+Elf32_Addr e_entry;
+Elf32_Off e_phoff;
+Elf32_Off e_shoff;
+Elf32_Word e_flags;
+Elf32_Half e_ehsize;
+Elf32_Half e_phentsize;
+Elf32_Half e_phnum;
+Elf32_Half e_shentsize;
+Elf32_Half e_shnum;
+Elf32_Half e_shstrndx;
+} Elf32_Ehdr;
+
+typedef uint64_t Elf64_Addr;
+typedef uint16_t Elf64_Half;
+typedef uint64_t Elf64_Off;
+typedef  int32_t Elf64_Sword;
+typedef uint32_t Elf64_Word;
+typedef uint64_t Elf64_Dword;
+
+typedef struct {
+unsigned char e_ident[EI_NIDENT];
+Elf64_Half e_type;
+Elf64_Half e_machine;
+Elf64_Word e_version;
+Elf64_Addr e_entry;
+Elf64_Off e_phoff;
+Elf64_Off e_shoff;
+Elf64_Word e_flags;
+Elf64_Half e_ehsize;
+Elf64_Half e_phentsize;
+Elf64_Half e_phnum;
+Elf64_Half e_shentsize;
+Elf64_Half e_shnum;
+Elf64_Half e_shstrndx;
+} Elf64_Ehdr;
+
+typedef struct {
+Elf32_Word p_type;
+Elf32_Off p_offset;
+Elf32_Addr p_vaddr;
+Elf32_Addr p_paddr;
+Elf32_Word p_filesz;
+Elf32_Word p_memsz;
+Elf32_Word p_flags;
+Elf32_Word p_align;
+} Elf32_Phdr;
+
+typedef struct {
+Elf64_Word p_type;
+Elf64_Word p_flags;
+Elf64_Off p_offset;
+Elf64_Addr p_vaddr;
+Elf64_Addr p_paddr;
+Elf64_Off p_filesz;
+Elf64_Off p_memsz;
+Elf64_Dword p_align;
+} Elf64_Phdr;
+
+bool is_ELF(const char * const addr);
+char get_ELF_class(const char * const addr);
+char get_ELF_endianness(const char * const addr);
+void elf32_build_program_image(const char *const elf);
+void elf64_build_program_image(const char *const elf);
+Elf32_Addr get_elf32_entrypoint(const char *const elf);
+Elf64_Addr get_elf64_entrypoint(const char *const elf);
+
+// helpers
+Elf32_Addr get_elf32_lowest_addr(const char *const elf);
+Elf64_Addr get_elf64_lowest_addr(const char *const elf);
+
+#endif
