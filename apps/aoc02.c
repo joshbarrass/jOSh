1f51c2c4a9c789fa2099a917477d3b766c7fb1f9
Joshua Barrass <josh.barrass.work@gmail.com>
2025-12-02 16:53:21 +0000

    Advent of Code Day 02 Part 2

--- apps/aoc02.c
+++ apps/aoc02.c
@@ -47,22 +47,63 @@ bool is_num_invalid(const unsigned long long int num) {
   sprintf(buf, "%lld", num);
   
   const size_t l = strlen(buf);
-  if (l % 2 != 0) return false;
+  if (l == 1) return false;
 
-  // copy the first half of the string to its own buffer
-  char front[(8 * sizeof(unsigned long long int)) * 30103 / 100000 + 5] = { 0 };
-  for (size_t i = 0; i < l / 2; ++i) {
-    front[i] = buf[i];
+  // calculate the length of the longest substring that could be
+  // repeated
+  size_t max_substr_length = 1;
+  for (size_t i = 1; i <= l/2; ++i) {
+    if (l % i == 0) max_substr_length = i;
   }
 
-  return strcmp(buf + l/2, front) == 0;
+  // check all lengths of substring for invalid options
+  static char substr[(8 * sizeof(unsigned long long int)) * 30103 / 100000 + 5] = { 0 };
+  static char chunk[(8 * sizeof(unsigned long long int)) * 30103 / 100000 + 5] = { 0 };
+  for (size_t substr_l = 1; substr_l <= max_substr_length; ++substr_l) {
+    if (l % substr_l != 0) continue;
+
+    // clear buffers
+    for (size_t i = 0; i < sizeof(substr) / sizeof(substr[0]); ++i) {
+      substr[i] = 0;
+    }
+    for (size_t i = 0; i < sizeof(chunk) / sizeof(chunk[0]); ++i) {
+      chunk[i] = 0;
+    }
+
+    // copy the substring of this length into a buffer
+    for (size_t i = 0; i < substr_l; ++i) {
+      substr[i] = buf[i];
+    }
+
+    // loop through each chunk of the string, copy that location into
+    // a buffer, and compare with the substr
+    bool all_chunks_match = true;
+    for (size_t i = 0; i < l; i += substr_l) {
+      // build a chunk of length substr_l
+      for (size_t j = 0; j < substr_l; ++j) {
+        chunk[j] = buf[i+j];
+      }
+      /* printf("Chunk %zu (substr_l=%zu): %s\n", i, substr_l, chunk); */
+
+      // compare the chunk with the substring
+      if (strcmp(chunk, substr) != 0) {
+        all_chunks_match = false;
+        break;
+      }
+    }
+    if (all_chunks_match) return true;
+  }
+  return false;
 }
 
 unsigned long long int sum_invalid_IDs(const Range r) {
   unsigned long long int total = 0;
 
-  for (unsigned long long int i = r.start; i < r.end; ++i) {
-    if (is_num_invalid(i)) total += i;
+  for (unsigned long long int i = r.start; i <= r.end; ++i) {
+    if (is_num_invalid(i)) {
+      total += i;
+      /* printf("Invalid: %llu\n", i); */
+    }
   }
 
   return total;
