77380c78d953378cc64cd829968c7a7a8bae1375
Joshua Barrass <josh.barrass.work@gmail.com>
2025-12-04 00:03:09 +0000

    Copy input to dynamically-allocated memory to ensure null terminator

--- apps/aoc_common.c
+++ apps/aoc_common.c
@@ -10,11 +10,21 @@ static const char *empty_string = "\0";
 
 char *get_input() {
   const Mod *mods = get_mods(mis);
+  const Mod *input_mod = NULL;
   for (size_t i = 0; i < mis->mods_count; ++i) {
     const Mod *mod = mods + i;
     if (strcmp("input", get_mod_string(mod)) == 0) {
-      return (char*)get_mod_start(mod);
+      input_mod = mod;
     }
   }
-  return empty_string;
+  if (input_mod == NULL) return empty_string;
+  
+  // allocate sufficient space for the input file + a null terminator
+  const size_t required_len = (input_mod->mod_end - input_mod->mod_start) + 1;
+  size_t pages_required = required_len / PAGE_SIZE;
+  if (required_len % PAGE_SIZE != 0) ++pages_required;
+  char *new_page = vmm_kmap((uintptr_t)pmm_alloc_pages(pages_required), required_len, 0, 0);
+  memmove(new_page, (void*)(uintptr_t)input_mod->mod_start, required_len - 1);
+  new_page[required_len-1] = 0;
+  return new_page;
 }
