2ffa5d34cb5a57f3e1528ec24d1e9cc6bc5926d3
Joshua Barrass <josh.barrass.work@gmail.com>
2025-12-12 13:22:39 +0000

    Generate aoc.cfg dynamically -- no more manual updates!

---
+++ apps/gen_aoc_cfg.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+
+import sys
+import os
+import re
+
+def parse_day(arg):
+    if os.path.splitext(arg)[-1].lower() != ".elf":
+        return None
+    m = re.search("aoc(\\d\\d).elf", arg)
+    if not m:
+        print("Bad match")
+        return None
+    return int(m.groups()[0])
+
+def main():
+    cfg_file = sys.argv[1]
+    assert os.path.splitext(cfg_file)[-1].lower() not in [".py", ".c", ".h", ".elf", ".ld"]
+    if os.path.exists(cfg_file):
+        with open(cfg_file, "rb") as f:
+            assert len(f.read()) == 0
+
+    args = sorted(sys.argv[2:])
+    with open(cfg_file, "w") as f:
+        for arg in args:
+            day = parse_day(arg)
+            if day is None:
+                continue
+            f.write(
+f"""menuentry "Day {day:02}" {{
+	multiboot /boot/jOShload.elf
+        module /boot/jOSh.elf "KERNEL.ELF"
+        module /aoc{day:02}.elf "init.elf"
+        module /inputs/day{day} "input"
+}}
+
+"""
+                )
+
+if __name__ == "__main__":
+    main()
