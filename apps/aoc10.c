d938e24a6bb006ad73d48486d76577eef569692b
Joshua Barrass <josh.barrass.work@gmail.com>
2025-12-22 15:38:43 +0000

    Finishes in an acceptable amount of time
    ...but fails (answer too low?!)

--- apps/aoc10.c
+++ apps/aoc10.c
@@ -439,7 +439,7 @@ static void lineq_reduce(LinEq *e) {
     }
 
     // try to get a diagonal value with unit magnitude
-    int old_diag = diag;
+    /* int old_diag = diag; */
     if (diag != 1 && diag != -1) {
       for (size_t col = row + 1; col < e->eqns.cols - 1; ++col) {
         diag = matrix_get(&e->eqns, row, col);
@@ -452,7 +452,7 @@ static void lineq_reduce(LinEq *e) {
           break;
         }
       }
-      if (diag != 1 && diag != -1) diag = old_diag;
+      if (diag != 1 && diag != -1) return; // failed to simplify further -- hope for the best?
     }
 
     // ensure the diagonal is positive
@@ -487,7 +487,7 @@ static void press_buttons_joltage(const Machine *m, joltage_t *state, const int 
   }
 }
 
-static int find_fewest_buttons_joltage(const Machine *m) {
+static int find_fewest_buttons_joltage(const Machine *m, const bool recalc) {
   // convert the machine to a system of linear equations
   LinEq eq = machine_to_lineq(m);
   lineq_reduce(&eq);
@@ -527,7 +527,7 @@ static int find_fewest_buttons_joltage(const Machine *m) {
   /* printf("Have %zu degrees of freedom\n", dof); */
 
   // recalculate max presses according to the new values
-  calculate_max_presses(&eq);
+  if (recalc) calculate_max_presses(&eq);
 
   // use a generator to brute force just the degrees of freedom
   int min_presses = INT32_MAX;
@@ -581,6 +581,7 @@ static int find_fewest_buttons_joltage(const Machine *m) {
       /* printf("Min now %d\n", min_presses); */
     }
   }
+  if (min_presses == INT32_MAX && recalc) return find_fewest_buttons_joltage(m, false);
 
   return min_presses;
 }
@@ -642,13 +643,16 @@ int main() {
       printf("=");
     }
     
-    const int b = find_fewest_buttons_joltage(&machines[i]);
+    const int b = find_fewest_buttons_joltage(&machines[i], true);
+    if (b == INT32_MAX) {
+      printf("Failed on %zu\n", i);
+    }
     /* printf("Machine %zu solveable with %d buttons\n", i+1, b); */
     total += b;
   }
   printf("\nPart 2 Total: %ld\n", total);
 
-  /* printf("Min: %d\n", find_fewest_buttons_joltage(&machines[24])); */
+  /* printf("Min: %d\n", find_fewest_buttons_joltage(&machines[47], true)); */
 
   return 0;
 }
