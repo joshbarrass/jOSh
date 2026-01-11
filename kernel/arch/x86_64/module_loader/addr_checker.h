bc112bf50a517059f5b371e146493fea469659b9
Joshua Barrass <josh.barrass.work@gmail.com>
2026-01-11 14:30:01 +0000

    Completely remove multiboot1 support

--- kernel/arch/x86_64/module_loader/addr_checker.h
+++ kernel/arch/x86_64/module_loader/addr_checker.h
@@ -1,14 +1,9 @@
 #ifndef ADDR_CHECKER_H
 #define ADDR_CHECKER_H 1
 
-#include "multiboot.h"
 #include <multiboot2.h>
-#include "stdbool.h"
-#include "stddef.h"
-#include "stdint.h"
+#include <stdint.h>
 
-uint64_t get_MIS_max_addr(const MIS *mis);
 uint64_t get_M2IS_max_addr(const M2IS *mis);
-bool check_all(const uint64_t load_addr, const MIS *mis);
 
 #endif
