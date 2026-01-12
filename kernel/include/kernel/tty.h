c6b30b35b1a0319b7a7eebe0510ab0534b23aede
Joshua Barrass <josh.barrass.work@gmail.com>
2026-01-12 00:03:43 +0000

    Add wrappers to call the kernel console driver's functions

--- kernel/include/kernel/tty.h
+++ kernel/include/kernel/tty.h
@@ -14,6 +14,13 @@ extern "C" {
   ConsoleDriver *get_kernel_console_driver();
   void set_kernel_console_driver(ConsoleDriver drv);
 
+  void term_set_color(const CharColor color);
+  void term_set_fg(const int fg);
+  void term_set_bg(const int bg);
+  void term_clear();
+  void term_draw_bitmap(const ScreenChar *bitmap, const size_t x, const size_t y,
+                        const size_t w, const size_t h);
+
 #ifdef __cplusplus
 }
 #endif
--- kernel/tty.c
+++ kernel/tty.c
@@ -10,3 +10,24 @@ ConsoleDriver *get_kernel_console_driver() {
 void set_kernel_console_driver(ConsoleDriver drv) {
   console_driver = drv;
 }
+
+void term_set_color(const CharColor color) {
+  return console_driver.set_color(&console_driver, color);
+}
+
+void term_set_fg(const int fg) {
+  return console_driver.set_fg(&console_driver, fg);
+}
+
+void term_set_bg(const int bg) {
+  return console_driver.set_bg(&console_driver, bg);
+}
+
+void term_clear() {
+  return console_driver.clear(&console_driver);
+}
+
+void term_draw_bitmap(const ScreenChar *bitmap, const size_t x, const size_t y,
+                      const size_t w, const size_t h) {
+  return console_driver.draw_bitmap(&console_driver, bitmap, x, y, w, h);
+}
