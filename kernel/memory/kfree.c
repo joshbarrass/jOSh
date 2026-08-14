efbd86e396a3efd2f033311799c6e94dc176abea
Joshua Barrass <josh.barrass.work@gmail.com>
2026-08-15 00:18:41 +0100

    Replace memory clobber fence with volatile
    Instead of creating a barrier where the compiler cannot make
    assumptions about the state of memory when crossing it, we explicitly
    tell the compiler that header is volatile and it cannot make
    assumptions about its memory accesses. The effect should be
    equivalent, but the latter is a much narrower scope, allowing the
    compiler to optimise the surrounding code properly whilst preventing
    it from making assumptions about the contents of the header.

--- kernel/memory/kfree.c
+++ kernel/memory/kfree.c
@@ -4,17 +4,12 @@
 #include <kernel/memory/vmm.h>
 #include <kernel/panic.h>
 
-static void _kfree_fullpage(const malloc_header_t *header) {
+static void _kfree_fullpage(volatile const malloc_header_t *header) {
   // copy everything to local variables before unmapping -- we don't
   // want the compiler to access the header again after it's been
   // unmapped.
   const phys_addr_t phys_page = vmm_get_phys((virt_addr_t)header);
   const size_t malloc_size = header->size;
-  // memory clobber fence to guard the above variables against
-  // optimisation -- compiler cannot assume that *header->size still
-  // holds the same value, so cannot optimise out the read of
-  // malloc_size
-  __asm__ volatile("" ::: "memory");
 
   // TODO: should we zero the page before unmapping it?
   vmm_kunmap((virt_addr_t)header, malloc_size);
